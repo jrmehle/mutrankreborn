ec37193d556401d1caba4af603a898c82dae36a4
Jared Mehle <jrmehle@gmail.com>
2018-03-19 22:47:35 -0500

    Use UUIDs for primary IDs in the database

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,12 @@ module Mutreborn
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.generators do |g|
+      g.orm :active_record, primary_key_type: :uuid
+    end
+
+
+    config.autoload_paths << "#{Rails.root}/app/rankers"
   end
 end
