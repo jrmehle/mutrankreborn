c5f150b0025367b955278a405babcb907ff343e8
Jared Mehle <jrmehle@gmail.com>
2018-06-19 21:18:23 -0500

    Spec coverage for team service class

--- app/services/team.rb
+++ app/services/team.rb
@@ -1,43 +1,43 @@
 class Team
   TEAMS = {
-    arizona: 'Cardinals',
-    atlanta: 'Falcons',
-    baltimore: 'Ravens',
-    buffalo: 'Bills',
-    carolina: 'Panthers',
-    chicago: 'Bears',
-    cincinnati: 'Bengals',
-    cleveland: 'Browns',
-    dallas: 'Cowboys',
-    detroit: 'Lions',
-    denver: 'Broncos',
-    green_bay: 'Packers',
-    houston: 'Texans',
-    indianapolis: 'Colts',
-    jacksonville: 'Jaguars',
-    kansas_city: 'Chiefs',
-    los_angeles: 'Rams',
-    miami: 'Dolphins',
-    new_england: 'Patriots',
-    new_orleans: 'Saints',
-    new_york_1: 'Giants',
-    new_york_2: 'Jets',
-    oakland: 'Raiders',
-    philadelphia: 'Eagles',
-    pittsburgh: 'Steelers',
+    arizona:       'Cardinals',
+    atlanta:       'Falcons',
+    baltimore:     'Ravens',
+    buffalo:       'Bills',
+    carolina:      'Panthers',
+    chicago:       'Bears',
+    cincinnati:    'Bengals',
+    cleveland:     'Browns',
+    dallas:        'Cowboys',
+    detroit:       'Lions',
+    denver:        'Broncos',
+    green_bay:     'Packers',
+    houston:       'Texans',
+    indianapolis:  'Colts',
+    jacksonville:  'Jaguars',
+    kansas_city:   'Chiefs',
+    los_angeles:   'Rams',
+    miami:         'Dolphins',
+    new_england:   'Patriots',
+    new_orleans:   'Saints',
+    new_york_1:    'Giants',
+    new_york_2:    'Jets',
+    oakland:       'Raiders',
+    philadelphia:  'Eagles',
+    pittsburgh:    'Steelers',
     san_francisco: '49ers',
-    seattle: 'Seahawks',
-    tampa_bay: 'Buccaneers',
-    tennessee: 'Titans',
-    minnesota: 'Vikings',
-    washington: 'Redskins'
+    seattle:       'Seahawks',
+    tampa_bay:     'Buccaneers',
+    tennessee:     'Titans',
+    minnesota:     'Vikings',
+    washington:    'Redskins'
   }
 
   def self.city_and_team_name(team_city_or_team_name: )
     if TEAMS.keys.include? team_city_or_team_name
       [team_city_or_team_name.to_s.titleize, TEAMS[team_city_or_team_name]].join(' ')
     elsif TEAMS.values.include? team_city_or_team_name
-      [TEAMS.key(team_city_or_team_name), team_city_or_team_name.to_s.titleize].join(' ')
+      [TEAMS.key(team_city_or_team_name).to_s.titleize, team_city_or_team_name.to_s.titleize].join(' ')
     end
 
   end
